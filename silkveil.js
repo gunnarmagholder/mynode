6aba984e31764844e0953d107a2734cadcd0d3ab
Gunnar Magholder <gmagholder@gmail.com>
2013-07-11 14:22:57 +0200

    working on silkveil.js

--- silkveil.js
+++ silkveil.js
@@ -12,6 +12,23 @@ var mappings = {
 		forceDownload: false
 	}
 };
+var actions = {
+	'download': function (res, mapping) {
+	},
+	'error': function (res,mapping) {
+		res.writeHead(mapping.statusCode, {
+			'Content-Type': 'text/html'
+		});
+		res.end(mapping.statusCode + ' ' + mapping.data);
+	},
+	'redirect': function (res, mapping) {
+		var statusCode = (mapping.type === 'permanent') ? 301 : 307;
+		res.writeHead(statusCode, {
+			'Location', mapping.url
+		});
+		res.end();
+	}
+};
 http.createServer(function(req,res) {
 	var alias = req.url.substring(1); 
 	var mapping = mappings[alias] || {
